6d376477bc8ee1f32b8f1919230fe36befb1cfe1
Imran Irfan <imranbirfan@gmail.com>
2025-02-24 15:08:18 -0800

    more testing

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 database_manager = db.DatabaseManagement()
 database_manager.connect()
 # Basic Todo Object
-#todo_test_object = models.ToDo(id=0, title='test', description='test', deadline=datetime.now(), status='Done')
+todo_test_object = models.ToDo(id=0, title='testing', description='testing', deadline=datetime.now(), status='Done')
+
 # Test Add Todo
 #database_manager.add_todo(todo_test_object)
-database_manager.delete_todo(33622889)
-all_todos = database_manager.view_all_todos()
+#database_manager.delete_todo(33622889)
+#database_manager.update_todo(230471354, todo_test_object)
+#all_todos = database_manager.view_all_todos()
+database_manager.delete_all_todos()
 
 @app.route('/')
 def hello_world():
