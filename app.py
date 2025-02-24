d16c5232c0f25f24c7592e54db450d818996d4e0
Imran Irfan <imranbirfan@gmail.com>
2025-02-24 15:11:47 -0800

    Add comments about testing

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ app = Flask(__name__)
 
 database_manager = db.DatabaseManagement()
 database_manager.connect()
-# Basic Todo Object
-todo_test_object = models.ToDo(id=0, title='testing', description='testing', deadline=datetime.now(), status='Done')
 
+## Testing, please ignore
+# Basic Todo Object
+#todo_test_object = models.ToDo(id=0, title='testing', description='testing', deadline=datetime.now(), status='Done')
 # Test Add Todo
 #database_manager.add_todo(todo_test_object)
 #database_manager.delete_todo(33622889)
 #database_manager.update_todo(230471354, todo_test_object)
 #all_todos = database_manager.view_all_todos()
-database_manager.delete_all_todos()
+#database_manager.delete_all_todos()
 
 @app.route('/')
 def hello_world():
