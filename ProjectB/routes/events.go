3c3327a35138ab74f6ea38655c6eb771a50772a0
Imran Irfan <imranbirfan@gmail.com>
2025-02-26 15:28:07 -0800

    Add update event function to routing

--- ProjectB/routes/events.go
+++ ProjectB/routes/events.go
@@ -63,3 +63,33 @@ func getEvent(context *gin.Context) {
 
 	context.JSON(http.StatusOK, event)
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event with supplied ID"})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event with supplied ID"})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.BindJSON(&updatedEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event with supplied ID"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated", "event": updatedEvent})
+}
