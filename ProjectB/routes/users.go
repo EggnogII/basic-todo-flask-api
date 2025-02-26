e7d05c09159dd4ac4caffe7dcd90bc251b979bb2
Imran Irfan <imranbirfan@gmail.com>
2025-02-26 14:37:03 -0800

    Add login function

--- ProjectB/routes/users.go
+++ ProjectB/routes/users.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"www.example.com/rest-api-proj/models"
+	"www.example.com/rest-api-proj/tools"
 )
 
 func signup(context *gin.Context) {
@@ -20,4 +21,30 @@ func signup(context *gin.Context) {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create user"})
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "User created", "user": user})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.BindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	// Generate token since we will be using that to authenticate the user
+	token, err := tools.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login Success", "token": token})
+
 }
