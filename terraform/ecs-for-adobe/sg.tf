a23255f257dc0a337a524af1ca1385d53dd636a1
Imran Irfan <imranbirfan@gmail.com>
2025-02-25 16:51:25 -0800

    Add ALB sg and update ecs_sg inbound

--- terraform/ecs-for-adobe/sg.tf
+++ terraform/ecs-for-adobe/sg.tf
@@ -3,6 +3,12 @@ resource "aws_security_group" "ecs_sg" {
     description = "Allow ECS tasks to comminicate with RDS"
     vpc_id = "vpc-096851120f324fbf5" # Change to var later
 
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "-1"
+        security_groups = [aws_security_group.alb_sg.id]
+    }
     egress {
         from_port = 0
         to_port = 0
@@ -21,5 +27,25 @@ resource "aws_security_group" "rds_sg" {
         to_port = 5432
         protocol = "tcp"
         security_groups = [aws_security_group.ecs_sg.id]
+    }
+}
+
+resource "aws_security_group" "alb_sg" {
+    name = "alb-sg"
+    description = "Allow inbound HTTP traffic"
+    vpc_id = "vpc-096851120f324fbf5"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
