4db689a448373ba28e7f90781367d3c7cdc64b6e
Imran Irfan <imranbirfan@gmail.com>
2025-02-26 13:42:07 -0800

    Modularize creation of tables

--- ProjectB/db/db.go
+++ ProjectB/db/db.go
@@ -46,6 +46,11 @@ func InitDB() {
 }
 
 func createTables() {
+	createUsersTable()
+	createEventsTable()
+}
+
+func createUsersTable() {
 	createUsersTable := `
 	CREATE TABLE IF NOT EXISTS users (
 	 id SERIAL PRIMARY KEY,
@@ -59,4 +64,25 @@ func createTables() {
 		fmt.Print(err)
 		panic("Could not create users table")
 	}
+}
+
+func createEventsTable() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    location TEXT NOT NULL,
+    datetime TIMESTAMP NOT NULL,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+`
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		fmt.Print(err)
+		panic("Could not create events table")
+	}
+
 }
