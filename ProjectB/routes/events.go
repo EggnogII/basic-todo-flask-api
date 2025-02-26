ddbf6d961f61ce2449b427ecd82c1dfef9cdb2a6
Imran Irfan <imranbirfan@gmail.com>
2025-02-26 15:19:47 -0800

    Add routing function for getting a specific Event

--- ProjectB/routes/events.go
+++ ProjectB/routes/events.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"www.example.com/rest-api-proj/models"
@@ -45,4 +46,20 @@ func getEvents(context *gin.Context) {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events"})
 	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event with supplied ID"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event with supplied ID"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
