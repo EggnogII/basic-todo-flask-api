a9f1f18bc06f3de906167edd11711ba0b292cf14
Imran Irfan <imranbirfan@gmail.com>
2025-02-26 11:56:23 -0800

    Refactor with more variables

--- terraform/rds-for-adobe/outputs.tf
+++ terraform/rds-for-adobe/outputs.tf
@@ -1,7 +1,11 @@
 output "cluster_endpoint" {
-    value = aws_rds_cluster.aurora_postgres_cluster.endpoint
+  value = aws_rds_cluster.aurora_postgres_cluster.endpoint
 }
 
 output "reader_endpoint" {
-    value = aws_rds_cluster.aurora_postgres_cluster.reader_endpoint
+  value = aws_rds_cluster.aurora_postgres_cluster.reader_endpoint
+}
+
+output "instance_endpoint" {
+  value = aws_rds_cluster_instance.aurora_postgres_instance.*.endpoint
 }
---
+++ terraform/rds-for-adobe/sg.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "aurora_sg" {
+  name        = "aurora-sg"
+  description = "Allow inbound PostgresSQL traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
