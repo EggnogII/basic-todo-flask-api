e91bcd3c6ea1dd553589faa7a925eff811b59b7e
Imran Irfan <imranbirfan@gmail.com>
2025-02-25 16:38:50 -0800

    Add variables

--- terraform/ecs-for-adobe/variables.tf
+++ terraform/ecs-for-adobe/variables.tf
@@ -1,3 +1,15 @@
 variable "rds_cluster_password" {
   type = string
 }
+
+variable "rds_endpoint" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
