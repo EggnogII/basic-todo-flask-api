bfdf220013b8d27bde117e4377aa82ab8562cd6e
Imran Irfan <imranbirfan@gmail.com>
2025-02-25 16:56:59 -0800

    Update ingress again

--- terraform/ecs-for-adobe/sg.tf
+++ terraform/ecs-for-adobe/sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "ecs_sg" {
     ingress {
         from_port = 80
         to_port = 80
-        protocol = "-1"
+        protocol = "tcp"
         security_groups = [aws_security_group.alb_sg.id]
     }
     egress {
