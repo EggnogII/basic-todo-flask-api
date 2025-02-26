000bca87c9f3ad9b7f5cfe3dae7653a11591adc1
Imran Irfan <imranbirfan@gmail.com>
2025-02-26 14:39:12 -0800

    Add gin so we can actually start the server

--- ProjectB/main.go
+++ ProjectB/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"www.example.com/rest-api-proj/db"
+	"www.example.com/rest-api-proj/routes"
 )
 
 func main() {
@@ -17,4 +19,7 @@ func main() {
 		user.Password = "password"
 		user.Save()
 	*/
+	server := gin.Default()
+	routes.RegisterRoutes(server)
+	server.Run(":8080")
 }
